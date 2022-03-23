0f4b56bf09d7dcfbf14d7559a9016599a35be04c
SvetoslavSO <98047767+SvetoslavSO@users.noreply.github.com>
2022-03-23 13:52:34 +0300

    Delete my_project_beats directory

--- my_project_beats/js/team.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll(".team__employee-name");
-for(i = 0; i < buttons.length; i++){
-  const button = buttons[i];
-  button.addEventListener("click", function(e){
-    e.preventDefault();
-    
-  })
-
-}
