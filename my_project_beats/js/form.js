9e70c676912a11785d101a2e15a73ff33eee2168
SvetoslavSO <98047767+SvetoslavSO@users.noreply.github.com>
2022-03-25 22:17:28 +0300

    Delete my_project_beats directory

--- my_project_beats/js/form.js
+++
@@ -1,7 +0,0 @@
-const myForm = document.querySelector("#form");
-const sendButton = document.querySelector("#arrange-delivery");
-
-sendButton.addEventListener("click", function(e){
-  e.preventDefault();
-  
-});
