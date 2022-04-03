c96e2f4b6ae6167691718c66fa63e71befb0b013
SvetoslavSO <98047767+SvetoslavSO@users.noreply.github.com>
2022-04-03 14:26:49 +0300

    Add files via upload

---
+++ gulp.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  SRC_PATH : "css",
+  DIST_PATH : "dist",
+  STYLES_LIBS:[
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+    'node_modules/bxslider/dist/jquery.bxslider.css',
+    'scss/main.scss'
+  ],
+  JS_LIBS:[
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/mobile-detect/mobile-detect.min.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js",
+    "node_modules/bxslider/dist/jquery.bxslider.min.js",
+    "js/*.js"
+  ]
+}
