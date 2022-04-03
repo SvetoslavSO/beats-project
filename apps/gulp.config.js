387a98529f76a1bd549a3a82e9060742b1c10cce
SvetoslavSO <98047767+SvetoslavSO@users.noreply.github.com>
2022-04-03 11:43:36 +0300

    Add files via upload

---
+++ apps/gulp.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  SRC_PATH : "css",
+  DIST_PATH : "apps",
+  STYLES_LIBS:[
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/bxslider/src/css/jquery.bxslider.css',
+    'node_modules/@fancyapps/fancybox/src/css',
+    'scss/main.scss'
+  ],
+  JS_LIBS:[
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/mobile-detect/mobile-detect.min.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js",
+    "node_modules/bxslider/dist/jquery.bxslider.min.js",
+    "js/*.js"
+  ]
+}
---
+++ apps/gulpfile.js
@@ -0,0 +1,71 @@
+const {src, dest, task, series, watch} = require('gulp');
+const rm = require( 'gulp-rm' );
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const sassGlob = require('gulp-sass-glob');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const autoprefixer = require('gulp-autoprefixer');
+const gcmq = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const {DIST_PATH, SRC_PATH, STYLES_LIBS, JS_LIBS} = require('./gulp.config');
+
+
+task( 'clean', () => {
+  return src( `${DIST_PATH}/**/*`, { read: false }).pipe(rm());
+});
+
+task('copy:html', () => {
+  return src('index.html')
+  .pipe(dest(DIST_PATH))
+  .pipe(reload({stream:true}));
+});
+
+task('server', () => {
+  browserSync.init({
+      server: {
+          baseDir: `${DIST_PATH}`
+      },
+      open: false
+  });
+});
+
+
+task('styles', () => {
+  return src([...STYLES_LIBS, 'scss/components/*.scss'])
+  .pipe(concat('main.min.scss'))
+  .pipe(sassGlob())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    cascade: true
+  }))
+  .pipe(gcmq())
+  .pipe(dest(`${DIST_PATH}/${SRC_PATH}`))
+  .pipe(reload({stream : true}))
+});
+
+task('photos', ()=>{
+  return src('beats-pictures/*').pipe(dest(`${DIST_PATH}/beats-pictures`));
+})
+
+task('sprite', ()=>{
+  return src ('sprite.svg').pipe(dest(`${DIST_PATH}`));
+})
+
+task('scripts', ()=>{
+  return src([...JS_LIBS, 'js/*.js'])
+    .pipe(concat('main.min.js', {newLine:';'}))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(dest(`${DIST_PATH}`))
+    .pipe(reload({stream : true}))
+})
+
+
+watch('scss/**/*.scss', series('styles'));
+watch('index.html', series('copy:html'));
+watch('js/*.js', series ('scripts'));
+task('default', series('clean',  'copy:html', 'photos', 'sprite' ,'styles', 'scripts', 'server'));
